d23248cb09aa6a24a4ca8336018f5f22ea5653e1
Erick Dantas <erick.dantas.work@gmail.com>
2023-01-02 16:46:16 -0300

    script

---
+++ aula02/ex004.js
@@ -0,0 +1,4 @@
+var nome = window.prompt('Qual é seu nome???')
+        document.write(`Olá, ${nome}! Seu nome tem ${nome.length} letras! <br>`)
+        document.write(`Seu nome em maiusculas é ${nome.toUpperCase()} <br>`)
+        document.write(`Seu nome em minusculas é ${nome.toLowerCase()}`)
