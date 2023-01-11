8f843cce34a9d5929323d0659cb816e23834bc50
Erick Dantas <erick.dantas.work@gmail.com>
2023-01-11 16:11:31 -0300

    ex011.js

---
+++ aula06/ex010.js
@@ -0,0 +1,10 @@
+var agora = new Date ()
+var hora = 1
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora < 12) {
+    console.log(`Bom Dia !!!`)
+} else if (hora <= 18) {
+    console.log(`Boa tarde !!!`)
+} else {
+    console.log(`Boa noite !!!`)
+}
