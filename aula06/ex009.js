5851766349393c5306d25fa10fcc23782d1b0b15
Erick Dantas <erick.dantas.work@gmail.com>
2023-01-17 15:17:19 -0300

    Update ex009.js

--- aula06/ex009.js
+++ aula06/ex009.js
@@ -1,6 +1,6 @@
 var idade = 66
 console.log(`Você tem ${idade} anos !!!`)
-if (idade < 16) {
+if (idade < 18) {
     console.log(`Não vota`)
 } else if (idade < 18 || idade > 65) {
     console.log(`Voto opcional`)
