c54b56ad71b2999af2e385f5da4b10f233c355f0
Erick Dantas <erick.dantas.work@gmail.com>
2023-01-20 00:04:38 -0300

    course finished

---
+++ aula013/ex027.js
@@ -0,0 +1,9 @@
+let amigo = {nome: 'José',
+sexo: 'M',
+peso: 82.9,
+engordar (p=0){
+    console.log('Engordou')
+    this.peso += p
+}}
+amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso}kg !!!`)
